2669d7633b5f49f4c7ab5d99fe7d59c3806050fc
fmcauley <frank.mcauley@gmail.com>
2025-01-02 14:09:41 -0500

    Working but the code needs to be cleaned up and I need to understand how refs work

--- two_sum/src/lib.rs
+++ two_sum/src/lib.rs
@@ -1,6 +1,20 @@
 
 pub fn find_sum_index(nums: Vec<i32>, target: i32) -> Vec<i32> {
-    return vec!(0);
+    let mut prior_index = 0;
+    let mut prior = 0;
+    let mut answer : Vec<i32> = vec!();
+
+    for (index, value) in nums.into_iter().enumerate() {
+        if target - value == prior {
+           answer.push(prior_index);
+           answer.push(index.try_into().unwrap());
+           break;
+        }
+        prior = value;
+        prior_index = index as i32;
+    
+    }
+    return answer;
 }
 
 #[cfg(test)]
@@ -13,6 +27,14 @@ mod tests {
         let target = 3;
         let output = find_sum_index(nums,target);
         assert_eq!(output, vec!(0,1));
+    }
+
+    #[test]
+    fn given_a_new_vector_and_a_target_find_index() {
+        let nums = vec!(1,2,4,5,6,7);
+        let target = 11;
+        let output = find_sum_index(nums,target);
+        assert_eq!(output, vec!(3,4));
     }
 
 }
--- two_sum/src/main.rs
+++ two_sum/src/main.rs
@@ -1,3 +1,6 @@
+use two_sum::find_sum_index;
+
 fn main() {
-    
+    let output = find_sum_index(vec!(1,7,9), 16);
+    println!("THIS IS A TEST {:?}", output);
 }
